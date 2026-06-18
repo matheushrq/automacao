8d4e8abe65dd62593ab4423406576f9a0db014e1
Matheus Henrique <henriquem320@gmail.com>
2026-06-17 23:55:08 -0300

    main: Criação de classe e ajuste da função de busca e download da planilha do site.

--- selenium/demonstrativo_magalu/automacao.py
+++ selenium/demonstrativo_magalu/automacao.py
@@ -1,19 +1,18 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 def inicia_navegador():
     options = webdriver.ChromeOptions()
-    options.add_argument('--start-maximized')
-    options.add_argument('--disable-notifications')
-    options.add_argument('--disable-infobars')
-    
     navegador = webdriver.Chrome(
         service = Service(ChromeDriverManager().install()), 
         options = options
     )
+    navegador.maximize_window() # inicia o navegador maximizado
     return navegador
 
 driver = inicia_navegador()
@@ -25,17 +24,24 @@ class AutomacaoMagalu:
     def acessa_site(self):
         url = "https://ri.magazineluiza.com.br/"
         self.driver.get(url)
+        time.sleep(5) # espera 5 segundos para a página carregar completamente
 
     def clica_link_planilha(self):
-        xpath_pag_inicial = '//*[@id="collapseMobile-3"]/ul/li[2]/a' # sempre coloca o xpath entre aspas simples
+        # sempre coloca o xpath entre aspas simples
+        xpath_pag_inicial = '//*[@id="collapseMobile-3"]/ul/li[2]/a'
         xpath_link = '//*[@id="BvNjWeiZirwEyUUBFST0Iw=="]'
 
-        wait = WebDriverWait(self.driver, 10)
-        link_inicial = wait.until(lambda d: d.find_element(By.XPATH, xpath_pag_inicial))
-        link_inicial.click()
+        link_pagina = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_pag_inicial))
+        )
+        self.driver.execute_script("arguments[0].click();", link_pagina) # clica no link usando JavaScript
+        time.sleep(5)
 
-        link_planilha = wait.until(lambda d: d.find_element(By.XPATH, xpath_link))
-        link_planilha.click()
+        link_planilha = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, xpath_link))
+        )
+        self.driver.execute_script("arguments[0].click();", link_planilha) # clica no link usando JavaScript
+        time.sleep(5)
 
     def fecha_navegador(self):
         self.driver.quit()
